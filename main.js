015f0daf0d8b9f14dd0ce4ce90ca941d4d9134ff
Gaurav singh <69497931+gaurav13021998@users.noreply.github.com>
2021-06-08 09:35:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+'strict'
+console.log('hello');
+const section1 = document.querySelector('#contact');
+document.querySelector('.btn').addEventListener('click',function(){
+    section1.scrollIntoView({behavior:'smooth'});
+});
+
+const allContainer = document.querySelectorAll('.container');
+const revealContainer = function(entries, observer){
+    const [entry] = entries;
+    if(!entry.isIntersecting) return;
+
+    entry.target.classList.toggle('section--hidden');
+
+    observer.unobserve(entry.target);
+}
+
+const containerObserver = new IntersectionObserver(revealContainer,{
+    root: null,
+    threshold: 0.25,
+})
+
+allContainer.forEach(el => {
+    containerObserver.observe(el);
+    el.classList.add('section--hidden');
+})
+
